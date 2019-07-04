375057b421c500eafcd35bb42458031ac13d0d2f
AKllX <felipekf@outlook.com>
2019-07-04 13:22:53 -0300

    Teste

---
+++ Radios/RadioType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrioServer.Radios
+{
+    public enum RadioType
+    {
+        QR,
+        QB,
+        MR,
+        E
+    }
+}
