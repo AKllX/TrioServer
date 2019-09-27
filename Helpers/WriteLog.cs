738ea6fa371d995a23943520e8eadf7ca0fad52c
lailasr <39624524+lailasr@users.noreply.github.com>
2019-09-27 14:19:52 -0300

    Add files via upload

---
+++ Helpers/WriteLog.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+using TrioServer.Radios;
+using TrioServer.Communication;
+
+namespace TrioServer.Helpers
+{
+    public static class WriteLog
+    {
+        public static void AppendLog(RadioMessage m)
+        {
+            using (StreamWriter outputFile = new StreamWriter(Path.Combine(Environment.CurrentDirectory, "WriteLines.txt"), true))
+            {
+                outputFile.WriteLine(m.RadioSerialNumber + ": " + BitConverter.ToString(m.FullPacket));
+            }
+        }
+    }
+}
